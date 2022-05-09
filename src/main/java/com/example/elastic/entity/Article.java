7071253fdc5ceb08e95964cbca653042b41c8770
Serendipity <jacksparrow414@163.com>
2022-05-09 21:40:24 +0800

    init project

--- src/main/java/com/example/elastic/entity/Article.java
+++ src/main/java/com/example/elastic/entity/Article.java
@@ -13,6 +13,7 @@ import lombok.Data;
  * @author jacksparrow414
  * @date 2022/5/9
  *
+ * Kibana中配置时区，Kibana->Advanced Settings-> Time zone选UTC而不是默认的Browser
  * PUT _index_template/article-template
  * {
  *   "index_patterns":["article-*"],
@@ -26,6 +27,7 @@ import lombok.Data;
  *       "properties": {
  *         "create_time": {
  *           "type": "date",
+ *           // 必须设置格式，否则Kibana中展示不出数据，会报[XXX is not a configured]
  *           "format": "yyyy-MM-dd'T'HH:mm:ss.SSS"
  *         },
  *         "article_content": {
@@ -45,6 +47,7 @@ public class Article {
     @JsonProperty(value = "create_time")
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
+    // 设置日期格式化为ISO8601格式
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime createTime;
     
