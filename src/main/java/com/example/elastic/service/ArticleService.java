a3be8fb3e2da8d5164554695a00d2ea1a30ccfc5
Serendipity <jacksparrow414@163.com>
2022-05-24 20:14:36 +0800

    log elasticsearch request

--- src/main/java/com/example/elastic/service/ArticleService.java
+++ src/main/java/com/example/elastic/service/ArticleService.java
@@ -1,10 +1,24 @@
 package com.example.elastic.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import com.example.elastic.entity.Article;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.apache.http.client.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +26,7 @@ import org.springframework.stereotype.Service;
  * @author jacksparrow414
  * @date 2022/5/9
  */
+@Log4j2
 @Service
 public class ArticleService {
     
@@ -27,5 +42,41 @@ public class ArticleService {
             .index(INDEX_PREFIX + indexName).document(article).build();
         IndexResponse index = elasticsearchClient.index(indexRequest);
         return index.id();
+    }
+    
+    @SneakyThrows
+    public List<String> simpleSearchArticle(String queryContent) {
+        List<String> result = new ArrayList<>();
+        Query query = QueryBuilders.bool().must(m -> m.match(mt -> mt.field("article_content").query(queryContent))).build()._toQuery();
+        SearchRequest searchRequest = new SearchRequest.Builder()
+            .index(INDEX_PREFIX+"*")
+            .query(query)
+            .source(s-> s.filter(f-> f.includes("article_content")))
+            .build();
+        SearchResponse<Article> search = elasticsearchClient.search(searchRequest, Article.class);
+        List<Hit<Article>> hits = search.hits().hits();
+        hits.forEach(item -> {
+            result.add(item.id());
+            log.info(item.source().getArticleContent());
+        });
+        return result;
+    }
+    
+    @SneakyThrows
+    public String maxDate() {
+        SearchResponse<Void> search =
+            elasticsearchClient.search(b -> b.index(INDEX_PREFIX + "*").size(0)
+                .aggregations("max_date",
+                    a -> a.max(m -> m.field("create_time").format("yyyy-MM-dd"))), Void.class);
+        return search.aggregations().get("max_date").max().valueAsString();
+    }
+    
+    @SneakyThrows
+    private void separate(String maxDate) {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        calendar.setTime(simpleDateFormat.parse(maxDate));
+        Calendar calendar1 = Calendar.getInstance();
+        int i = calendar.compareTo(calendar1);
     }
 }
--- src/test/java/com/example/elastic/service/ArticleServiceTest.java
+++ src/test/java/com/example/elastic/service/ArticleServiceTest.java
@@ -3,6 +3,8 @@ package com.example.elastic.service;
 import com.example.elastic.entity.Article;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,11 +26,23 @@ public class ArticleServiceTest {
     public void assertIndexArticle() {
         Article article = new Article();
         article.setCreateTime(LocalDateTime.now());
-        article.setArticleContent("this is an article");
-        article.setAuthor("jack");
+        article.setArticleContent("This ARTICLE ahhah");
+        article.setAuthor("oop");
         ObjectMapper objectMapper = new ObjectMapper();
         System.out.println(objectMapper.writeValueAsString(article));
-                String articleId = articleService.indexArticle(article);
+        String articleId = articleService.indexArticle(article);
         Assertions.assertNotNull(articleId);
+    }
+    
+    @Test
+    public void assertSearchArticle() {
+        List<String> result = articleService.simpleSearchArticle("article");
+        Assertions.assertTrue(result.size() > 0);
+    }
+    
+    @Test
+    public void assertMaxDate() {
+        String result = articleService.maxDate();
+        Assertions.assertNotNull(result);
     }
 }
